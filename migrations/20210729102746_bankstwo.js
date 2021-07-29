f7a2e99dca00556fe002f02c0ca021ac75ddee0a
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-07-29 10:57:49 -0600

    reverting to day5-take1

---
+++ migrations/20210729102746_bankstwo.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('banks', t => {
+        t.increments();
+        t.string('name');
+        t.string('icon');
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('banks');
+};
